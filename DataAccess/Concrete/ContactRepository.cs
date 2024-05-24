940cc7e72c9a6150b49bf9c6bda01695e5a53969
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2024-05-24 23:46:08 +0400

    Create all repository classes and interfaces for entities

---
+++ DataAccess/Concrete/ContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.Concrete.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.DbContexts;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+	public class ContactRepository : EfEntityRepository<BlogDbContext, Contact>, IContactRepository
+	{
+	}
+}
