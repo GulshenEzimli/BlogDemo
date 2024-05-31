393dd9f2655f7825ddb3b360f29c936ac790bb2d
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2024-05-31 09:46:04 +0400

    Add ArticleController and its views

---
+++ BlogUI/Controllers/ArticleController.cs
@@ -0,0 +1,28 @@
+﻿using BlogUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogUI.Controllers
+{
+	public class ArticleController : Controller
+	{
+		private readonly IArticleModelService _articleService;
+
+		public ArticleController(IArticleModelService articleService)
+		{
+			_articleService = articleService;
+		}
+
+		public IActionResult Index()
+		{
+			var articleModels = _articleService.GetAllArticles();
+			
+			return View(articleModels);
+		}
+
+		public IActionResult ArticleDetails(int id)
+		{
+			var articleModel = _articleService.GetAllArticles().FirstOrDefault(x => x.Id == id);
+			return View(articleModel);
+		}
+	}
+}
