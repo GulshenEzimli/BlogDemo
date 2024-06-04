b9208541dc386352607161a454fcce1db5574186
GulshenEzimli <gulshn.azimli@gmail.com>
2024-06-04 16:56:47 +0400

    Add CategoryList viewcomponent

---
+++ BlogUI/Models/Category/CategoryCountsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogUI.Models.Category
+{
+	public class CategoryCountsModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Count { get; set; }
+	}
+}
---
+++ BlogUI/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,28 @@
+﻿using BlogUI.Models.Category;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogUI.ViewComponents.Category
+{
+	public class CategoryList : ViewComponent
+	{
+		private readonly ICategoryService _categoryService;
+		private readonly IArticleService _articleService;
+		public CategoryList(ICategoryService categoryService, IArticleService articleService)
+		{
+			_categoryService = categoryService;
+			_articleService = articleService;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var categories = _categoryService.GetAll().Select(c => new CategoryCountsModel
+			{
+				Id = c.Id,
+				Name = c.Name,
+				Count = _articleService.GetAll().Where(a => a.CategoryId == c.Id).ToList().Count	
+			}).ToList();
+			return View(categories);
+		}
+	}
+}
