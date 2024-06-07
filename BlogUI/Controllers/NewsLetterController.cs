c725fbdffd4d83326200ccbb348e239629187e24
GulshenEzimli <gulshn.azimli@gmail.com>
2024-06-07 13:10:57 +0400

    Support Subscribe action get tuple objet for add newsletter

--- BlogUI/Controllers/NewsLetterController.cs
+++ BlogUI/Controllers/NewsLetterController.cs
@@ -1,14 +1,28 @@
-﻿using Entity.Concrete;
+﻿using BlogUI.Models.NewsLetter;
+using BlogUI.Services.Interfaces;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogUI.Controllers
 {
 	public class NewsLetterController : Controller
 	{
-		[HttpPost]
-		public IActionResult SubscribeMail(string mail)
+		private readonly INewsLetterModelService _service;
+
+		public NewsLetterController(INewsLetterModelService service)
 		{
-			return View();
+			_service = service;
+		}
+
+		[HttpPost]
+		public IActionResult SubscribeMail(NewsLetterModel model, int id)
+		{
+			//var newsLetterModel = new NewsLetterModel();
+			//newsLetterModel.Mail = mail;
+			//newsLetterModel.IsDeleted = false;
+			//_service.Add(newsLetterModel);
+
+			return RedirectToAction();
 		}
 	}
 }
