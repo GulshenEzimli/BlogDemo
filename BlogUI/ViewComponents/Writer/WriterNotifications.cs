96a2e3632fab0686a3c589f699cb515e540ffd97
GulshenEzimli <gulshn.azimli@gmail.com>
2024-06-21 00:36:32 +0400

    Add notifications and messages to componentModel

---
+++ BlogUI/ViewComponents/Writer/WriterNotifications.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace BlogUI.ViewComponents.Writer
+{
+    public class WriterNotifications : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
