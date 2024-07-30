4c4f818b7eef4ce37a04d6de02900ce3b7eb881f
GulshenEzimli <gulshn.azimli@gmail.com>
2024-07-30 14:46:46 +0400

    Add result structure

---
+++ Core/Results/Concrete/Result.cs
@@ -0,0 +1,24 @@
+﻿using Core.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            IsSuccess = success;
+        }
+        public bool IsSuccess { get; }
+        public string Message { get; }
+    }
+}
